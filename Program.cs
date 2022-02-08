20258fbf3b02086b583091ece816d391cc0cd6c4
Trunyashkaaaaaa <93324206+IljaTrunov@users.noreply.github.com>
2022-02-08 17:54:36 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,89 +6,96 @@ using System.Threading.Tasks;
 
 namespace CProgrammerimine
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            /*double hind;
-            hind = 10;
-            string pilet_tuup="täispilet";
-            Console.WriteLine("Mis on sinu nimi, vene okkupand?");
-            string eesnimi = Console.ReadLine();
-            Console.Write("Tere tulemast! {0}", eesnimi);
-            if (eesnimi.ToLower() == "okkupand")
-            {
-                Console.WriteLine("\nTule külla!, Kas sa tahab kino minna? ");
-                string vastus = Console.ReadLine();
-                if (vastus.ToUpper()=="JAH" || vastus.ToUpper()=="YES")
-                {
-                    Console.WriteLine("\nKui vana sa oled ");
-                    int vanus = int.Parse(Console.ReadLine());
-                    if (vanus < 0 || vanus > 108)
-                    {
-                        Console.WriteLine("Viga andmega!");
-                        pilet_tuup = "0, sa oled vana ja võib olla surnud.";
-                    }
-                    else if (vanus < 6 || vanus >= 65)
-                    {
-                        hind -= hind * 1;
-                        pilet_tuup = "tasuta";
-                    }
-                    else if (vanus >= 6 && vanus < 12)
-                    {
-                        hind -= hind * 0.3;
-                        pilet_tuup="See on laastepilet";
-                    }
-                    else if (vanus >= 12 && vanus < 18)
-                    {
-                        hind -= hind * 0.1;
-                        pilet_tuup = "See on täispilet";
-                    }
-                    char[] tahed = eesnimi.ToCharArray();
-                    int i = 0;
-                    eesnimi = "";
-                    foreach (var t in tahed) 
-                    {
-                        if (i==0)
-                        {
-                            eesnimi +=t.ToString().ToUpper();
-
-                        }
-                        else
-                        {
-                            eesnimi +=t.ToString().ToLower();
-                        }
-                        i += 1;
-                    }
-
-                    Console.WriteLine("{0}, sul on {1} vaja maksta {2}", eesnimi, hind, pilet_tuup);
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			//Console.WriteLine("Tere nimeta nime pinginaabri => ");
+			//string n1 = Console.ReadLine();
+			//Console.WriteLine("Nimeta nime pinginaabri => ");
+			//string n2 = Console.ReadLine();
+			//if ((n1 == "Mihhail" && n2 == "Marco") || (n2 == "Mihhail" && n1 == "Marco") || (n1 == "Edgar" && n2 == "Marco") || (n2 == "Edgar" && n1 == "Marco") || (n1 == "Aleksandr" && n2 == "Aleksandr") || (n1 == "Aleksandr" && n2 == "Daniil") || (n2 == "Aleksandr" && n1 == "Daniil") || (n1 == "Alina" && n2 == "Jelena") || (n2 == "Alina" && n1 == "Jelena") || (n1 == "Ervin" && n2 == "Vlademir") || (n2 == "Ervin" && n1 == "Vlademir"))
+			//{
+			//	Console.WriteLine("Nad on pinginaabrid!");
+			//}
+			//else
+			//{
+			//	Console.WriteLine("Nad ei ole pinginaabrid!");
+			//}
 
 
-                }
-                else
-                {
-                    Console.WriteLine("Mõtle valja ise...");
-                }
-            
-            }
-            else
-            {
-                Console.WriteLine("\nMind täna kodus pole! :( ");
-            }
-            Console.ReadKey();*/
 
-            string pinginaabrid ="((Artem && Timofei ) || (German && Kevin))";
-            Console.WriteLine("Mis on sinu pinginaaber täna on?");
-            string vastus=Console.ReadLine();
-            if (vastus.ToLower() == "artem" || vastus.ToUpper() == "Artem")
-            {
-                Console.WriteLine("Selge, sinu pinginaaber on ", pinginaabrid);
-            }
-            else if (vastus.ToLower() == "timofei" || vastus.ToUpper() == "TIMOFEI")
-            {
-                Console.WriteLine("Selge, sinu pinginaaber on Ilja!");
-            }
+			Console.ReadKey();
+		}
 
-        }
-    }
-}
+
+
+
+	}
+}	
+//			double hind = 15;
+//			string pilet_tuup = "tais";
+//			Console.WriteLine("Tere tulemast !!! Mis on sinu nimi ");//метод
+//			Console.Title="Pealkiri";//свойство 
+//			string nimi = Console.ReadLine();
+//			Console.WriteLine("Tere {0}",nimi);
+//			if (nimi.ToUpper()=="JUKU" || nimi.ToLower()=="alina")
+//			{
+//				Console.WriteLine($"Tule küla {nimi} Kas tahad kinno ");
+//				string vastus = Console.ReadLine();
+//				if (vastus.ToLower()=="jah"|| vastus.ToLower()=="yes")
+//				{
+//					Console.WriteLine("Kui vana sa oled?");
+//					int vanus = int.Parse(Console.ReadLine());
+//					if (vanus < 0 || vanus > 108)
+//					{
+//						Console.WriteLine("Viga andmega!");
+//						pilet_tuup = "0, sa oled vana ja võib olla surnud.";
+//					}
+//					else if (vanus < 6 || vanus >= 65)
+//					{
+//						hind -= hind * 1;
+//						pilet_tuup = "tasuta";
+//					}
+//					else if (vanus >= 6 && vanus < 12)
+//					{
+//						hind -= hind * 0.3;
+//						pilet_tuup = "See on laastepilet";
+//					}
+//					else if (vanus >= 12 && vanus < 18)
+//					{
+//						hind -= hind * 0.1;
+//						pilet_tuup = "See on täispilet";
+//					}
+//					char[] tahed = nimi.ToCharArray();//juku->['j','u','k','u']
+//					int i = 0;
+//					nimi = "";
+//					foreach (var t in tahed)
+//					{
+//						if (i == 0)
+//						{
+//							nimi += t.ToString().ToUpper();// программа делает букву большой
+//						}
+//						else
+//						{
+//							nimi += t.ToString().ToLower();
+//						}
+//						i++;
+//					}
+//						Console.WriteLine("{0},sul on {2}pilet vaja maksata {1}", nimi, hind,pilet_tuup);
+//				}
+//				else
+//				{
+//					Console.WriteLine("Ei taha?!:(");
+//				}
+//			}
+//			else 
+//			{
+//				Console.WriteLine("Täna ma olen hõivatud! :(");
+//			}
+			
+//			Console.ReadKey();//всегда  в конце
+//		}
+//	}
+//}
